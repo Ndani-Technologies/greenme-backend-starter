8d45beef393f596f4e30e8e6f69ca53a465f2b8f
Martin Macharia <mosmartin5@gmail.com>
2023-05-06 00:30:07 +0200

    chore: add a healthcheck route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import healthcheckRoute from "./routes/healthcheck.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/v1/healthcheck", healthcheckRoute);
 app.use("/api/v1/users", userRoutes);
 
 app.use(notFoundMiddleware);
