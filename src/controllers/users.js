02cfc3f3e622416f677fe66f7012f29f83a67e30
Martin Macharia <mosmartin5@gmail.com>
2023-05-16 23:06:51 +0200

    feat: add caching

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,26 +3,42 @@ import logger from "../middleware/logger.js";
 import User from "../models/users.js";
 import asyncHandler from "../middleware/async.js";
 import ErrorResponse from "../utils/error-response.js";
+import redis from "../configs/redis.js";
 
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
+  const usersCache = await redis.get("users");
+  if (usersCache) {
+    return res
+      .status(200)
+      .json({ success: true, data: JSON.parse(usersCache) });
+  }
+
+  const users = await User.find();
+  await redis.set("users", JSON.stringify(users), "EX", 60);
   res.status(200).json({ success: true, data: users });
 });
 
 const createUser = asyncHandler(async (req, res) => {
   const user = await User.create(req.body);
+  await redis.del("users");
   res.status(201).json({ success: true, data: user });
   logger.info(`User created with id ${user.id}`);
 });
 
 const getUser = asyncHandler(async (req, res, next) => {
   const { id: userId } = req.params;
-  const user = await User.findOne({ _id: req.params.id });
 
+  const userCache = await redis.get(`user:${userId}`);
+  if (userCache) {
+    return res.status(200).json({ success: true, data: JSON.parse(userCache) });
+  }
+
+  const user = await User.findOne({ _id: userId });
   if (!user) {
     return next(new ErrorResponse(`No user with id ${userId}`, 404));
   }
 
+  await redis.set(`user:${userId}`, JSON.stringify(user), "EX", 60);
   res.status(200).json({ success: true, data: user });
 });
 
